30b9abc339374b33f4ce5e4dff9e260d482d7a22
gokayari <gokayari@gmail.com>
2022-07-05 22:26:33 +0200

    SQL - day4 self join

--- Queries/day4/self_join.sql
+++ Queries/day4/self_join.sql
@@ -3,6 +3,11 @@
 select employee_id,first_name,last_name,manager_id from employees;
 
 select e1.employee_id,e1.first_name,e1.last_name,e1.manager_id,e2.employee_id,e2.first_name,e2.last_name
-from employees e1 left join employees e2
+from employees e1 join employees e2
+on e1.manager_id = e2.employee_id
+order by e1.employee_id;
+
+select e1.employee_id,e1.first_name,e1.last_name,e1.manager_id,e2.employee_id,e2.first_name,e2.last_name
+from employees e1 left join employees e2 --with boss"Steven King"
 on e1.manager_id = e2.employee_id
 order by e1.employee_id;
