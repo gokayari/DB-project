3e63903776ccf4a7c5cfa4d6845128b53c3eaabf
gokayari <gokayari@gmail.com>
2022-07-05 21:53:59 +0200

    SQL - day3-4

--- Queries/day3/join2.sql
+++ Queries/day3/join2.sql
@@ -5,3 +5,12 @@ select * from departments;
 select first_name, last_name, DEPARTMENT_NAME
 from employees e left join departments d
 on e.DEPARTMENT_ID = d.DEPARTMENT_ID;
+
+Select customer_id, first_name, last_name, customer.address_id, address.address_id address, phone
+FROM customer LEFT OUTER JOIN address
+ON customer.address_id = address.address_id
+WHERE customer.address_id IS NULL;
+
+SELECT * FROM customer FULL OUTER JOIN address
+ON customer.address_id = address.address_id
+WHERE customer.address_id IS NULL OR address.address_id IS NULL;
